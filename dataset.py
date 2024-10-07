fa248db5cc83f5bafd218cf7ead3d5d6521f842f
Parth Sharma <86831956+Parth-nXp@users.noreply.github.com>
2024-10-07 16:41:16 +0530

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,35 @@
+import cv2
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+
+class BrainMRIDataset(Dataset):
+    def __init__(self, dataframe, image_transform=None, mask_transform=None, target_size=(256, 256)):
+        self.dataframe = dataframe
+        self.image_transform = image_transform
+        self.mask_transform = mask_transform
+        self.target_size = target_size
+        
+    def __len__(self):
+        return len(self.dataframe)
+    
+    def __getitem__(self, idx):
+        image_path = self.dataframe.iloc[idx]['image_filename']
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask_path = self.dataframe.iloc[idx]['mask_images']
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        
+        image = cv2.resize(image, self.target_size)
+        mask = cv2.resize(mask, self.target_size)
+        
+        image = Image.fromarray(image)
+        mask = Image.fromarray(mask)
+        
+        if self.image_transform:
+            image = self.image_transform(image)
+        if self.mask_transform:
+            mask = self.mask_transform(mask)
+            
+        mask = mask.unsqueeze(0)
+        return image, mask
